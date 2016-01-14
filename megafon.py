130aac827225c022550f122ef45aaed658882ff4
themylogin <themylogin@gmail.com>
2016-01-15 00:48:41 +0600

    2captcha for megafon

--- megafon.py
+++ megafon.py
@@ -4,41 +4,57 @@
 from __future__ import unicode_literals
 
 from bs4 import BeautifulSoup
+from captcha2upload import CaptchaUpload
 from datetime import datetime, timedelta
+import logging
 import re
 import requests
+import sys
+import tempfile
 
+from themylog.client import setup_logging_handler
 from themylog.collector.time_series import TimeSeries
 from themylog.collector.utils.storage import Storage
 
+setup_logging_handler("%s.collector" % sys.argv[1])
+
 storage = Storage()
 
-if storage.get("captcha", datetime.min) > datetime.now() - timedelta(hours=24):
-    storage["cookies"] = {}
-    raise Exception("Waiting for captcha to disappear")
+response = requests.get("https://lk.megafon.ru/", cookies=storage.get("cookies", None))
+if "Сервис временно недоступен" in response.text:
+    raise Exception("Сервис временно недоступен")
+if "/login/" in response.url:
+    csrf_request = response
+    auth_data = data={"CSRF": re.search('CSRF_PARAM = "(.+)"', csrf_request.text).group(1),
+                      "j_username": "<телефон>",
+                      "j_password": "<пароль от сервис-гида>"}
 
-page = requests.get("https://lk.megafon.ru/", cookies=storage.get("cookies", None)).text
-if "private-office-header-balans" not in page:
-    csrf_request = requests.get("https://lk.megafon.ru/login/")
-    if "captcha-img" in csrf_request.text:
-        storage["captcha"] = datetime.now()
-        raise Exception("There is captcha")
+    soup = BeautifulSoup(csrf_request.text)
+    captcha = soup.find("img", "captcha-img")
+    if captcha:
+        captcha_request = requests.get("https://lk.megafon.ru" + captcha["src"],
+                                       cookies=csrf_request.cookies)
+        with tempfile.NamedTemporaryFile(suffix=".png") as f:
+            f.write(captcha_request.content)
+            f.flush()
+
+            captcha = CaptchaUpload("<2captcha.com key>", logging.getLogger("captcha"), 30)
+            auth_data["captcha"] = captcha.solve(f.name)
 
     page_request = requests.post("https://lk.megafon.ru/dologin/",
                                  cookies=csrf_request.cookies,
-                                 data={"CSRF": re.search('CSRF_PARAM = "(.+)"', csrf_request.text).group(1),
-                                       "j_username": "<телефон>",
-                                       "j_password": "<пароль от сервис-гида>"})
+                                 data=auth_data)
     page = page_request.text
     if "captcha-img" in page:
-        storage["captcha"] = datetime.now()
-        raise Exception("There is captcha")
+        raise Exception("Captcha was solved incorrectly")
 
     storage["cookies"] = {cookie.name: cookie.value for cookie in page_request.cookies}
+else:
+    page = response.text
 
 time_series = TimeSeries()
 soup = BeautifulSoup(page)
 time_series.balance({
-    "balance":  float(re.sub(r"[^0-9\.]", "", soup.find("div", "private-office-header-balans").get_text().replace(",", "."))),
+    "balance":  float(re.sub(r"[^0-9\.]", "", soup(text=re.compile("Баланс"))[0].next.get_text().replace(",", "."))),
     "bonus":    float(re.sub(r"[^0-9\.]", "", soup.find("div", "private-office-width").get_text().replace(",", "."))),
 })
